95d2f0c9dc105d23c81ca12a53afe8a424d027cf
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-18 00:31:56 +0530

    fix: course names

--- data/demo-videos.ts
+++ data/demo-videos.ts
@@ -1,10 +1,10 @@
 const demoVideos = [
     {
-        course: 'DHAWNI Bhushan',
+        course: 'DHAWNI Bhusan',
         url: "https://player.vimeo.com/video/1128196760?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
     },
     {
-        course: 'DHAWNI Bhibhushan',
+        course: 'DHAWNI Bibhusan',
         url: 'https://player.vimeo.com/video/1127703307?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'
     }
 ]
