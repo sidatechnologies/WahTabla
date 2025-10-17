4b2d7cf96ed42badda9d99a750eec0dcd57fd44a
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-18 01:05:54 +0530

    fix: shit issues

--- data/demo-videos.ts
+++ data/demo-videos.ts
@@ -1,10 +1,10 @@
 const demoVideos = [
     {
-        course: 'DHAWNI Bhusan',
+        course: 'DHWANI Bhusan',
         url: "https://player.vimeo.com/video/1128196760?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
     },
     {
-        course: 'DHAWNI Bibhusan',
+        course: 'DHWANI Bibhusan',
         url: 'https://player.vimeo.com/video/1127703307?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'
     }
 ]
