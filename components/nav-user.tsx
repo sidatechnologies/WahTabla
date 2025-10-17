dc25b6d645094c2e171c875ca631dd0cfc24e909
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-10-17 21:04:28 +0530

    fix: gender sorting

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -74,7 +74,7 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground border"
             >
               <Avatar className="h-10 w-10 rounded-lg">
-                <AvatarImage src={'/avatar-male.png'} alt={isPending
+                <AvatarImage src={profileData?.data?.gender === 'male' ? '/avatar-male.png' : '/avatar-female.png'} alt={isPending
                   ? "Loading..."
                   : profileData?.data?.username
                     ? profileData.data.username
@@ -105,7 +105,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-10 w-10 rounded-lg">
-                  <AvatarImage src={'/avatar-male.png'} alt={isPending
+                  <AvatarImage src={profileData?.data?.gender === 'male' ? '/avatar-male.png' : '/avatar-female.png'} alt={isPending
                     ? "Loading..."
                     : profileData?.data?.username
                       ? profileData.data.username
