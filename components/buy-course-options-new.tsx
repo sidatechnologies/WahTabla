4764f2cd6b1fe1f7a4b4b081aaf551f52b564286
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-17 00:00:56 +0530

    fix: razorpay indian pricing

--- components/buy-course-options-new.tsx
+++ components/buy-course-options-new.tsx
@@ -356,7 +356,7 @@ const BuyingOptionsNew = ({ course, userLastPurchase, user }: propsType) => {
                         handleCheckout(
                           course.name,
                           planLabel,
-                          priceModel.amount,
+                          country === "IN" ? priceModel.amountIn : priceModel.amount,
                           priceModel.type
                         );
                       }}
